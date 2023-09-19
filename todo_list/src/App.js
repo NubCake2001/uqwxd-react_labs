3fa346ca2a6418881032c526618d396675c7c00d
NubCake2001 <nubcake2001@gmail.com>
2023-09-19 23:22:58 +0530

    From MAC

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState},{useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
@@ -18,8 +18,8 @@ const App = () => {
 
   useEffect(() => {
     if (todos.length > 0) {
-        const json = JSON.stringify(todos);
-        localStorage.setItem("todos", json);
+      const json = JSON.stringify(todos);
+      localStorage.setItem("todos", json);
     }
   }, [todos]);
 
@@ -31,14 +31,12 @@ const App = () => {
       text: todo.trim(),
       completed: false,
     };
-    if (newTodo.text.length > 0 ) {
-        setTodos([...todos].concat(newTodo));
-        setTodo("");
-
+    if (newTodo.text.length > 0) {
+      setTodos([...todos].concat(newTodo));
+      setTodo("");
     } else {
-
-        alert("Enter Valid Task");
-        setTodo("");
+      alert("Enter Valid Task");
+      setTodo("");
     }
   }
   function deleteTodo(id) {
@@ -60,59 +58,58 @@ const App = () => {
     const updatedTodos = [...todos].map((todo) => {
       if (todo.id === id) {
         todo.text = editingText;
-        }
-        return todo;
-      });
-      setTodos(updatedTodos);
-      setTodoEditing(null);
-    }
-
-    return (
-        <div id="todo-list">
-          <h1>Todo List</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              onChange={(e) => setTodo(e.target.value)}
-              value={todo}
-            />
-            <button type="submit">Add Todo</button>
-          </form>
-{todos.map((todo) => (
-  <div key={todo.id} className="todo">
-    <div className="todo-text">
-      {/* Add checkbox for toggle complete */}
-      <input
-        type="checkbox"
-        id="completed"
-        checked={todo.completed}
-        onChange={() => toggleComplete(todo.id)}
-      />
-      {/* if it is edit mode, display input box, else display text */}
-      {todo.id === todoEditing ?
-        (<input
-          type="text"
-          onChange={(e) => setEditingText(e.target.value)}
-        />) :
-        (<div>{todo.text}</div>)
       }
-    </div>
-    <div className="todo-actions">
-      {/* if it is edit mode, allow submit edit, else allow edit */}
-      {todo.id === todoEditing ?
-      (
-        <button onClick={() => submitEdits(todo.id)}>Submit Edits</button>
-      ) :
-      (
-        <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
-      )}
+      return todo;
+    });
+    setTodos(updatedTodos);
+    setTodoEditing(null);
+  }
 
-      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-     </div>
-  </div>
-))}
+  return (
+    <div id="todo-list">
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      {todos.map((todo) => (
+        <div key={todo.id} className="todo">
+          <div className="todo-text">
+
+            <input
+              type="checkbox"
+              id="completed"
+              checked={todo.completed}
+              onChange={() => toggleComplete(todo.id)}
+            />
+
+            {todo.id === todoEditing ? (
+              <input
+                type="text"
+                onChange={(e) => setEditingText(e.target.value)}
+              />
+            ) : (
+              <div>{todo.text}</div>
+            )}
+          </div>
+          <div className="todo-actions">
+            {/* if it is edit mode, allow submit edit, else allow edit */}
+            {todo.id === todoEditing ? (
+              <button onClick={() => submitEdits(todo.id)}>Submit Edits</button>
+            ) : (
+              <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
+            )}
+
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </div>
         </div>
-      );
-    };
+      ))}
+    </div>
+  );
+};
 
 export default App;
